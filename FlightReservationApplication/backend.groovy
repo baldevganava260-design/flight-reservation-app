2fa05342356eadb4afce9fca01bedbd905d502e2
Baldev <baldevganava260@gmail.com>
2026-02-19 15:37:56 +0530

    1st stage is added

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -3,7 +3,7 @@ pipeline{
     stages{
         stage('Code-pull'){
             steps{
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+                git branch: 'main', url: 'https://github.com/baldevganava260-design/flight-reservation-app.git'
             }
         }
     }
